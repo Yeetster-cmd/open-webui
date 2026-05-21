3dedec54a2132df7cf10f9bef03e5359d8f55def
Yeetster-cmd <theyeetster2@gmail.com>
2026-05-21 13:00:59 +0300

    feat: Increase contrast on hovered or selected chat-window.

--- src/lib/components/layout/Sidebar/ChatItem.svelte
+++ src/lib/components/layout/Sidebar/ChatItem.svelte
@@ -413,10 +413,10 @@
 			id="sidebar-chat-item"
 			class=" w-full flex justify-between rounded-xl px-[11px] py-[6px] {id === $chatId ||
 			confirmEdit
-				? 'bg-gray-100 dark:bg-gray-900 selected'
+				? 'bg-gray-200 dark:bg-gray-800 selected'
 				: selected
-					? 'bg-gray-100 dark:bg-gray-950 selected'
-					: 'group-hover:bg-gray-100 dark:group-hover:bg-gray-950'}  whitespace-nowrap text-ellipsis relative {generating
+					? 'bg-gray-200 dark:bg-gray-800 selected'
+					: 'group-hover:bg-gray-200 dark:group-hover:bg-gray-800'}  whitespace-nowrap text-ellipsis relative {generating
 				? 'cursor-not-allowed'
 				: ''}"
 		>
@@ -449,10 +449,10 @@
 			id="sidebar-chat-item"
 			class=" w-full flex justify-between rounded-xl px-[11px] py-[6px] {id === $chatId ||
 			confirmEdit
-				? 'bg-gray-100 dark:bg-gray-900 selected'
+				? 'bg-gray-200 dark:bg-gray-800 selected'
 				: selected
-					? 'bg-gray-100 dark:bg-gray-950 selected'
-					: ' group-hover:bg-gray-100 dark:group-hover:bg-gray-950'}  whitespace-nowrap text-ellipsis"
+					? 'bg-gray-200 dark:bg-gray-800 selected'
+					: ' group-hover:bg-gray-200 dark:group-hover:bg-gray-800'}  whitespace-nowrap text-ellipsis"
 			href="/c/{id}"
 			on:click={() => {
 				dispatch('select');
@@ -522,10 +522,10 @@
 		id="sidebar-chat-item-menu"
 		class="
         {id === $chatId || confirmEdit
-			? 'from-gray-100 dark:from-gray-900 selected'
+			? 'from-gray-200 dark:from-gray-800 selected'
 			: selected
-				? 'from-gray-100 dark:from-gray-950 selected'
-				: 'invisible group-hover:visible from-gray-100 dark:from-gray-950'}
+				? 'from-gray-200 dark:from-gray-800 selected'
+				: 'invisible group-hover:visible from-gray-200 dark:from-gray-800'}
             absolute {className === 'pr-2'
 			? 'right-[8px]'
 			: 'right-1'} top-[4px] py-1 pr-0.5 mr-1.5 pl-5 bg-linear-to-l from-80%
